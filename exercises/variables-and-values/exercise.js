b010e24d2065ad94544c22048d29488a0356f151
Sam Bleckley <sam@climatum.com>
2018-01-25 16:41:58 -0500

    Added exercise files for class #3.

---
+++ exercises/variables-and-values/exercise.js
@@ -0,0 +1,12 @@
+var counter = 0;
+const numberOfPeople = 9;
+let message = "Message!";
+
+const sam = {
+  firstName: "Sam",
+  lastName: "Bleckley",
+  luckyNumber: 2,
+  favoriteColor: "blue"
+};
+
+const names = ["Sam", "Eli", "Hanna", "Michael", "Emily", "Malcom", "Bree", "Jim", "Noble", "Vik"];
