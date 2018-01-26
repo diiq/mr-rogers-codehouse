771ce0e72f98f85229ccb06621f701e888df435c
NobleCar <noblejohnson1@hotmail.com>
2018-01-25 20:10:52 -0500

    nobleexercisejs2

--- exercises/variables-and-values/exercise.js
+++ exercises/variables-and-values/exercise.js
@@ -11,16 +11,16 @@ const sam = {
 
 const names = ["Sam", "Eli", "Hanna", "Michael", "Emily", "Malcom", "Bree", "Jim", "Noble", "Vik"];
 
-<<<<<<< 4cf7db75b46b3e2426889f55bb51e484df4e67a4
+
 const emily = {
   firstName: "Emily",
   lastName: "Westenbroek",
   luckyNumber: 16,
   favoriteColor: "green"
 };
-=======
+
 const noble = {
     firstname: "noble",
     lastname: "johnson",
   ssn:"nothing to see here"};
->>>>>>> noble-exercise
+t
