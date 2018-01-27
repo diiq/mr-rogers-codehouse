22f0aea2566cc9406c62c9d43c1d46a412376d45
Sam Bleckley <sam@climatum.com>
2018-01-27 08:00:00 -0500

    Lesson 4, functions and loops

---
+++ exercises/functions-and-loops/loops.js
@@ -0,0 +1,67 @@
+var counter = 0;
+const numberOfPeople = 9;
+let message = "Message!";
+
+const sam = {
+  firstName: "Sam",
+  lastName: "Bleckley",
+  luckyNumber: 2,
+  favoriteColor: "blue"
+};
+
+const vikram = {
+  firstName: "Vikram",
+  lastName: "Badarinath",
+  luckyNumber: 23,
+  favoriteColor: "Honolulu Blue"
+};
+
+const emily = {
+  firstName: "Emily",
+  lastName: "Westenbroek",
+  luckyNumber: 16,
+  favoriteColor: "green"
+};
+
+const noble = {
+    firstname: "noble",
+    lastname: "johnson",
+  ssn:"nothing to see here"
+};
+
+const michael = {
+  firstName: "Michael",
+  lastName: "Coluzzi",
+  luckyNumber: 7,
+  favoriteColor: "red"
+};
+
+const eli = {
+  firstName: "Eli",
+  lastName: "Cecil",
+  luckyNumber: 97,
+  favoriteColor: "blood red"
+};
+
+const bree = {
+  firstName: "Bree",
+  lastName: "Tanner",
+  luckyNumber: 7,
+  favoriteColor: "Millennial Pink"
+};
+
+const Malcolm = {
+	firstName: "Malcolm",
+	lastName:"Cusack",
+	luckyNumber: 7,
+	favoriteColor: "blue",
+};
+
+const jim = {
+  firstName: "JIm",
+  lastName: "Riley",
+  luckyNumber: "17",
+  favoriteColor: "grey"
+};
+
+const people = [sam, eli, hanna, michael, emily, malcom, bree, jim, noble, vik];
