648b39cf57b3353f9d86391bf17cf8ab977f54bf
breetanner <bree@studio-us.com>
2018-01-30 17:55:28 -0500

    First commit of in progress lab 2

---
+++ labs/lab-2-javascript-game/script.js
@@ -0,0 +1,19 @@
+//function startGame() {}
+const play = prompt("Would you like to play?");
+  if (play === "yes") {
+    const playerName = prompt("Enter you player name");
+    function startCombat()
+  }
+
+var grantHP = 10
+var playerHP = 40
+
+function startCombat() {
+  while (grantHP > 0) || (player HP > 0) {
+    (grantHP - 1 || grantHP - 2) && (playerHP - 1 || playerHP - 2)
+    document.write ("The Almighty Grant has" + grantHP + "health left.")
+    document.write (playerName + " has" + playerHP + "health left.")
+  }
+}
+// while player HP is > 0, -1 || -2
+// OR white grant HP is > 0, -1 || -2
