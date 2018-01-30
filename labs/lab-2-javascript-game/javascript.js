d6bd2b9fd4d0c98c80fb800368468174675685fb
ihomebrew2 <ihomebrew2@yahoo.com>
2018-01-29 20:47:04 -0500

    Cleaned up indents

--- labs/lab-2-javascript-game/javascript.js
+++ labs/lab-2-javascript-game/javascript.js
@@ -6,10 +6,10 @@ var playGame = true;
 
 function startGame() {
   var playGame = confirm("Shall we battle!");
-    if (playGame === true) {
-      var battleName = prompt("Enter your battle name!");
-      return startCombat(battleName);
-    } else alert("Return when you have obtained the courage to battle!!");
+  if (playGame === true) {
+    var battleName = prompt("Enter your battle name!");
+    return startCombat(battleName);
+  } else alert("Return when you have obtained the courage to battle!!");
 }
 startGame();
 
@@ -18,32 +18,31 @@ function getDamage(max) {
 }
 
 function startCombat(battleName) {
-
-while (playGame === true) {
- var battleAnswer = confirm("OK for attack, Cancel for retreat");
- //console.log(battleAnswer);
- if (battleAnswer === true) {
-   if (wins === 3) {
-     console.log("You beat Grant 3 times, and won the game!");
-     break;
-   }
-   grantHealth -= getDamage();
-   userHealth -= getDamage();
-   console.log(battleName + ", Your health is now " + userHealth);
-   console.log("Grants health is now " + grantHealth);
-   //console.log("Wins" + "" + wins);
-   if (grantHealth <= 0 ){
-     wins++;
-     grantHealth = 10;
-     console.log(" won round " + wins);
-   } else if (userHealth <= 0){
-     playGame = false;
-     console.log("You lose! Try again");
-   }
-  } else {
- alert("Coward! Grant has destroyed you!");
- console.log("Coward! Grant has destroyed you!");
- break;
- }
-}
+  while (playGame === true) {
+    var battleAnswer = confirm("OK for attack, Cancel for retreat");
+    //console.log(battleAnswer);
+    if (battleAnswer === true) {
+      if (wins === 3) {
+        console.log("You beat Grant 3 times, and won the game!");
+        break;
+      }
+      grantHealth -= getDamage();
+      userHealth -= getDamage();
+      console.log(battleName + ", Your health is now " + userHealth);
+      console.log("Grants health is now " + grantHealth);
+      //console.log("Wins" + "" + wins);
+      if (grantHealth <= 0 ){
+        wins++;
+        grantHealth = 10;
+        console.log(" won round " + wins);
+      } else if (userHealth <= 0){
+        playGame = false;
+        console.log("You lose! Try again");
+      }
+    } else {
+      alert("Coward! Grant has destroyed you!");
+      console.log("Coward! Grant has destroyed you!");
+      break;
+    }
+  }
 }
