3e742ea5399cee002da82f37c7374fd0cccc9755
ihomebrew2 <ihomebrew2@yahoo.com>
2018-01-29 19:33:20 -0500

    Having a bit of trouble with the startCombat function. It won't start. And it seems very clunky.

--- labs/lab-2-javascript-game/javascript.js
+++ labs/lab-2-javascript-game/javascript.js
@@ -1,47 +1,87 @@
 var userHealth = 40;
 var grantHealth = 10;
 var wins = 0;
-var battleName = "";
+var battleName;
+var playGame;
 
 
-
-var playGame = confirm("Shall we battle!"); {
-console.log(playGame);
-
-if (playGame === true){
-  var battleName = prompt("Enter your battle name!");
-  console.log(battleName);
-
-} else { alert("Return when you have obtained the courage to battle!!");
+function getDamage(max) {
+  return Math.floor(Math.random() * 5) + 1;
 }
+// console.log(getDamage());
+
+function startGame() {
+  var playGame = confirm("Shall we battle!");
+    if (playGame === true) {
+      var battleName = prompt("Enter your battle name!");
+      startCombat(battleName);
+    } else alert("Return when you have obtained the courage to battle!!");
 }
 
+
+function startCombat(battleName) {
   while (playGame === true) {
-    if (wins === 3) {
-      console.log("You beat Grant 3 times, and won the game!");
-      break;
-    }
-    grantHealth -= Math.floor((Math.random() *5) +1);
-    userHealth -= Math.floor((Math.random() *5) +1);
-    console.log(battleName + ", Your health is now " + userHealth);
-    console.log("Grants health is now " + grantHealth);
-    console.log("Wins" + "" + wins);
-    if (grantHealth <= 0 ){
-      wins++;
-      grantHealth = 10;
-      console.log("You won this round");
-    } else if (userHealth <= 0){
-      playGame = false;
-      console.log("You lose! Try again");
+    var battleAnswer = confirm("OK for attack, Cancel for retreat");
+    if (battleAnswer === true) {
+      if (wins === 3) {
+        console.log("You beat Grant 3 times, and won the game!");
+        break;
+      } else
+      grantHealth -= getDamage();
+      userHealth -= getDamage();
+      console.log(battleName + ", Your health is now " + userHealth);
+      console.log("Grants health is now " + grantHealth);
+      if (grantHealth <= 0 ){
+        wins++;
+        grantHealth = 10;
+        console.log(battleName + " won round " + wins);
+      } else if (userHealth <= 0){
+        playGame = false;
+        console.log("You lose! Try again");
+      } else {
+        console.log("Coward! Grant has destroyed you!");
+        break;
+      }
     }
   }
-    function getDamage(max) {
-      return Math.floor(Math.random() * 5) + 1;
-    }
+}
+
+// var playGame = confirm("Shall we battle!"); {
+// console.log(playGame);
+//
+// if (playGame === true){
+//   var battleName = prompt("Enter your battle name!");
+//   console.log(battleName);
+//
+// } else { alert("Return when you have obtained the courage to battle!!");
+// }
+// }
 
 
-
-
+  // while (playGame === true) {
+  //   if (wins === 3) {
+  //     console.log("You beat Grant 3 times, and won the game!");
+  //     break;
+  //   }
+  //   grantHealth -= getDamage();
+  //   userHealth -= getDamage();
+  //   console.log(battleName + ", Your health is now " + userHealth);
+  //   console.log("Grants health is now " + grantHealth);
+  //   console.log("Wins" + "" + wins);
+  //   if (grantHealth <= 0 ){
+  //     wins++;
+  //     grantHealth = 10;
+  //     console.log("You won this round");
+  //   } else if (userHealth <= 0){
+  //     playGame = false;
+  //     console.log("You lose! Try again");
+  //   }
+  // }
+  //
+  //
+  //
+  //
+  //
 
 //
 //
