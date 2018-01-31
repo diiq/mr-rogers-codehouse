72c6ee4ed83cc8165d50d95db2670885ab0bf4c2
NobleCar <noblejohnson1@hotmail.com>
2018-01-30 22:45:52 -0500

    unfishedlab2

---
+++ labs/lab-2-javascript-game/script.js
@@ -0,0 +1,30 @@
+function gameStart(){
+    const play = window.prompt("Do you want to play the game?");
+    if (play === "yes"){
+        var name =window.prompt("Please enter your name:");
+        startFight(name);
+    }
+
+}
+
+
+
+
+function startFight(){
+    var winTotal = 0;
+    var enemyHP = 15;
+    var userHP = 50;
+    var anotherAttack;
+    var rematch;
+
+
+    while(winTotal < 5 && (userHP > 0)){
+        rematch = window.prompt("Would you like to attack again?");
+        if (anotherAttack === "yes"){
+            userHP = userHP -
+
+
+        }
+    }
+
+  }
