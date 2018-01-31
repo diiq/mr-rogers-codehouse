da7e25a7968c2c68e215e4454c23242bb4215d53
mcoluzzi <mcoluzzi@visitops.com>
2018-01-31 14:52:24 -0500

    Added prompt to ask player if they would like to attack.

--- labs/lab-2-javascript-game/script.js
+++ labs/lab-2-javascript-game/script.js
@@ -3,17 +3,22 @@ var grantHealth = 10;
 var winCount = 0;
 
 function getDamage() {
-    return Math.floor(Math.random() * 5) + 1
+    return Math.floor(Math.random() * 5) + 1;
 }
 
-function startCombat(damage) {
-   console.log("You took ", damage, "damage!");
-   playerHealth = playerHealth - getDamage();
+function startCombat() {
+
+  const playerDmg = getDamage();
+  const grantDmg = getDamage();
+
+   console.log("You took ", playerDmg, "damage!");
+   playerHealth = playerHealth - playerDmg;
    console.log("You have health ", playerHealth, "left.");
 
-   console.log("Grant took ", damage, "damage!");
-   grantHealth = grantHealth - getDamage();
+   console.log("Grant took ", grantDmg, "damage!");
+   grantHealth = grantHealth - grantDmg;
    console.log("Grant has health ", grantHealth, "left.");
+
 }
 
 
@@ -23,16 +28,20 @@ function startGame() {
   if (play === "Yes") {
     var playerName = prompt("What is your name?");
     while (winCount < 3) {
-        startCombat();
-        if (grantHealth <= 0 && winCount < 3) {
-          winCount++;
-          grantHealth = 10;
-          console.log("You have defeated Grant ", winCount,"times.");
-        } else if (playerHealth <= 0) {
-          console.log("You have been defeated.");
-        } else if (winCount === 3) {
-          console.log("You have won the game!");
-          return
+        var continueGame = prompt("Do you want to attack again? (Attack or Quit)")
+        if (continueGame === "Attack") {
+          startCombat();
+          if (grantHealth <= 0 && winCount < 3) {
+            winCount++;
+            grantHealth = 10;
+            console.log("You have defeated Grant ", winCount,"times.");
+          } else if (playerHealth <= 0) {
+            console.log("You have been defeated.");
+            return;
+          } else if (winCount === 3) {
+            console.log("You have won the game!");
+            return;
+          }
         }
       }
     }
