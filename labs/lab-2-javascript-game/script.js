68942ebf88e793ad0d3b52e76da548861af3845c
breetanner <bree@studio-us.com>
2018-01-31 16:41:52 -0500

    playerName variable is now defined and gameEnd function now works, code has some questions commented out at bottom to ask in class

--- labs/lab-2-javascript-game/script.js
+++ labs/lab-2-javascript-game/script.js
@@ -1,20 +1,18 @@
-var grantHP = 10
-var playerHP = 40
-var winCount = 0
-
 function startGame() {
   const play = prompt("Would you like to play?");
   if (play === "yes") {
     var playerName = prompt("Enter you player name");
-    startCombat();
+    startCombat(playerName);
   }
 }
 
-function gameEnd() {
-  if (winCount === 3) {
+function gameEnd(playerHP, winCount) {
+  if ((winCount === 3) && (playerHP < 0)) {
+    console.log ("The game has ended with a draw.");
+  } else if (winCount === 3) {
     console.log ("You have defeated the Almighty Grant!!");
   } else if (playerHP <= 0) {
-    console.log ("YOU LOST!! The Almighty Grant has won, taken over the planet, and destoryed humanity.");
+    console.log ("YOU LOST!! The Almighty Grant has won, taken over the planet, and destroyed humanity.");
   }
 }
 
@@ -24,26 +22,38 @@ function getDamage() {
   return (Math.floor(Math.random() * (max - min + 1) + min));
 }
 
-function startCombat() {
+function startCombat(playerName) {
+  var grantHP = 10
+  var playerHP = 40
+  var winCount = 0
   while((winCount < 3) && (grantHP > 0) && (playerHP > 0)) {
-    var attackChoice = prompt("Would you like to attack or quit?"); // put attack and quit in ""
+    var attackChoice = prompt("Would you like to \"attack\" or \"quit\"?");
     if (attackChoice === "attack") {
       grantHP = grantHP - getDamage();
       playerHP = playerHP - getDamage();
       console.log ("The Almighty Grant has " + grantHP + " health left."); // still logs when Grant HP is less than 0
-      console.log ("PLACEHOLDER" + " has " + playerHP + " health left."); // could not get playerName to be defined
+      console.log (playerName + " has " + playerHP + " health left.");
       if (grantHP <= 0) {
         winCount = winCount + 1;
         grantHP = 10;
         console.log ("You have beat the Grant!" + " You need to win " + (3 - winCount) + " more round(s)." );
       }
-    } else if (attackChoice === "quit") {
-      console.log ("PLACEHOLDER has quit the game. " + "Remaining Health Points: " + playerHP + ", Total Wins: " + winCount);
-      break;
+    } else if (attackChoice === "quit") { // Ask Sam if there is any reason this is a bad idea
+      console.log (playerName + " has quit the game. " + "Remaining Health Points: " + playerHP + ", Total Wins: " + winCount);
+     break;
     }
   }
-  gameEnd()
+  gameEnd(playerHP, winCount);
 }
+
+
+// ________ ASK ABOUT THE FOLLOWING IN CLASS __________
+
+// break keyword
+
+// playerName // could not get playerName to be defined
+      // why does this need to be called out in the function parameters?
+
 
 // function startCombat() {
 //   var attackChoice = prompt("Would you like to attack or quit?"); // put attack and quit in ""
