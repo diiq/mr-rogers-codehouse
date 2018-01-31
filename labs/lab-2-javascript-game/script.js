8a3408f7cd520169d03760b0bda5e6e2c909037e
Malcolm Cusack <cusackmalk@gmail.com>
2018-01-31 18:16:08 -0500

    commit 2

--- labs/lab-2-javascript-game/script.js
+++ labs/lab-2-javascript-game/script.js
@@ -1,9 +1,70 @@
+var playerHP = 40;
+var grantHP = 10;
+var playersName = "";
+var grantDeaths = 0;
+
 function startGame() {
 	 wouldYou = prompt("Would you like to play?");
 
 	if (wouldYou === "yes") {
-		var playersName = prompt("What would you like to name your hero?");
+		playersName = prompt("What would you like to name your hero?");
+		
 		startCombat();
+	}
+}
+
+function startCombat() {
+	playerHP = 40;
+	grantDeaths = 0;
+	while (grantDeaths <= 3 && playerHP >= 0) {
+		 battle();
+	}
+	if (grantDeaths === 3) {
+	declareWinner(); 
+   }else if (playerHP <=0) {
+	declareWinner();
+	}
+
+}
+
+function battle() {
+	grantHP = 10;
+	while (grantHP >= 0 && playerHP >= 0) {
+		round();
+	}
+ 	if (grantHP <= 0) {
+	grantDeaths = grantDeaths + 1;
+	console.log(playersName + ' has won a round');
+	}
+
+}
+
+function round() {
+	
+	attack = prompt('Do you want to attack? or quit?')
+	if (attack === 'attack') {
+
+		while (playerHP >= 0 && grantHP >= 0) {
+
+			playerHP = playerHP - getDamage();
+			console.log(playersName + " " + "has" + " " + playerHP + " " + "health left");
+
+			grantHP = grantHP - getDamage();
+			console.log("Grant the Mighty Chicken has" + " " + grantHP + " " + "heath left");
+
+		}
+	} else if (attack === 'quit') {
+		startGame();
+	}
+}	
+
+function declareWinner() {
+	if (grantDeaths === 3) {
+		console.log(playersName + ' has won!!!');
+		startGame();
+	} else if (playerHP <= 0) {
+		console.log('Grant the Almighty has won ');
+		startGame();
 	}
 }
 
@@ -16,50 +77,39 @@ function getDamage() {
 }
 
 
-function round(playersName, winCount) {
-		var playersNameHP = 40;
-		var grantHP = 10;
-		var round = 0;
 
-		while (playersNameHP >= 0 && grantHP >= 0) {
-
-		playersNameHP = playersNameHP - getDamage();
-		console.log(playersName + " " + "has" + " " + playersNameHP + " " + "health left");
-
-		grantHP = grantHP - getDamage();
-		console.log("Grant the Mighty Chicken has" + " " + grantHP + " " + "heath left");
-
-		   winCount();
-		} 
-	}	
-
-
-
-function winCount(grantHP, playersName) {
-	if (grantHP >= 0) {
-		console.log(playersName + " " + "won a round");
-		 return  winCount ++ ;
-		grantHP = 10;	
-	}
-}
-	 
-function winner(playersName, winCount) {
-	if (winCount >= 3) {
-		console.log(playersName + " " + "wins!");
-	} else {
-		console.log("Grant the Almighty won :( ");
-
-	}
-
-}
+// var grantHealth;
+// var grantDeaths;
+// var userHealth;
+// var userName;
 	
-function startCombat() {
-	while (winCount() <= 3) {
-		 round();
-	}
-}
+// function playGame 
+// 	ask if wanted to play and name
 
-	
+// 	startCombat 
+
+
+// 	declare winner 
+
+
+//  StarCombat 
+// 	battle till grand dies 3 times or you die once
+
+//  battle 
+// 	grand gets health
+// 	skirmish till one players dead (health = 0)
+// 	record winner
+
+//  round  
+// 	ask if want to attack
+// 	if attack deal damage 
+// 	if quit, end battle
+
+
+
+
+
+
 
 
 
