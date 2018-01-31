3497bec770bebd86d7016534c68ac4cb6ae25a95
Sam Bleckley <sam@climatum.com>
2018-01-31 14:42:46 -0500

    Exercises for dom and events

---
+++ exercises/dom-and-events/exercise.js
@@ -0,0 +1,21 @@
+// Learning Goals:
+// After this lesson, you should be able to:
+// - Explain what a DOM element is
+// - Retrieve a DOM object for any element on the page
+// - Explain what an event handler is, and list several
+// - Attach event handler to any element on the page
+
+
+// A function that returns the value of the input
+
+
+// A function that returns the text of the button
+
+
+// A function that sets the greeting to be "Hello, [value of input]!"
+
+
+// A function that runs when the button is clicked (moused-over, focused, etc)
+
+
+// Onload.
