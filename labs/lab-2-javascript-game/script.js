b5ca8153d11832f02aa956292ea59c5b49b61ddc
breetanner <bree@studio-us.com>
2018-01-31 13:31:03 -0500

    Successfully got gameEnd function to work and added some personality to the game.

--- labs/lab-2-javascript-game/script.js
+++ labs/lab-2-javascript-game/script.js
@@ -12,10 +12,37 @@ function startGame() {
 
 function gameEnd() {
   if (winCount === 3) {
-    console.log ("You have defeated the Almighty Grant!");
+    console.log ("You have defeated the Almighty Grant!!");
   } else if (playerHP <= 0) {
-    console.log ("The Almighty Grant has won.");
+    console.log ("YOU LOST!! The Almighty Grant has won, taken over the planet, and destoryed humanity.");
   }
+}
+
+function getDamage() {
+  const min = 1;
+  const max = 5;
+  return (Math.floor(Math.random() * (max - min + 1) + min));
+}
+
+function startCombat() {
+  while((winCount < 3) && (grantHP > 0) && (playerHP > 0)) {
+    var attackChoice = prompt("Would you like to attack or quit?"); // put attack and quit in ""
+    if (attackChoice === "attack") {
+      grantHP = grantHP - getDamage();
+      playerHP = playerHP - getDamage();
+      console.log ("The Almighty Grant has " + grantHP + " health left."); // still logs when Grant HP is less than 0
+      console.log ("PLACEHOLDER" + " has " + playerHP + " health left."); // could not get playerName to be defined
+      if (grantHP <= 0) {
+        winCount = winCount + 1;
+        grantHP = 10;
+        console.log ("You have beat the Grant!" + " You need to win " + (3 - winCount) + " more round(s)." );
+      }
+    } else if (attackChoice === "quit") {
+      console.log ("PLACEHOLDER has quit the game. " + "Remaining Health Points: " + playerHP + ", Total Wins: " + winCount);
+      break;
+    }
+  }
+  gameEnd()
 }
 
 // function startCombat() {
@@ -38,64 +65,4 @@ function gameEnd() {
 //     break;
 //     }
 //   }
-// }
-
-
-function startCombat() {
-  while((winCount < 3) && (grantHP > 0) && (playerHP > 0)) {
-    var attackChoice = prompt("Would you like to attack or quit?"); // put attack and quit in ""
-    if (attackChoice === "attack") {
-      grantHP = grantHP - getDamage();
-      playerHP = playerHP - getDamage();
-      console.log ("The Almighty Grant has " + grantHP + " health left."); // still logs when Grant HP is less than 0
-      console.log ("PLACEHOLDER" + " has " + playerHP + " health left."); // could not get playerName to be defined
-      if (grantHP <= 0) {
-        winCount = winCount + 1;
-        grantHP = 10;
-        console.log ("You have beat the Grant!" + " You need to win " + (3 - winCount) + " more round(s)." );
-      } else {
-        gameEnd() // wont go to game end
-      }
-    } else if (attackChoice === "quit") {
-      console.log ("PLACEHOLDER" + userHP + winCount);
-      break;
-    }
-  }
-}
-
-function getDamage() {
-  const min = 1;
-  const max = 5;
-  return (Math.floor(Math.random() * (max - min + 1) + min));
-}
-
-
-
-
-// (grantHP - 1 || grantHP - 2) && (playerHP - 1 || playerHP - 2)
-
-
-
-
-
-
-
-
-
-
-
-// function getDamage() {
-//
-
-
-//
-
-
-// while player HP is > 0, -1 || -2
-// OR white grant HP is > 0, -1 || -2
-
-
-// const min = 1;
-// const max = 5;
-// return (Math.floor(Math.random() * (max - min + 1) + min));
 // }
