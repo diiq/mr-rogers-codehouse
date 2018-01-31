d695ea052ac4c2e19742361d4a8786c675b3a47c
NobleCar <noblejohnson1@hotmail.com>
2018-01-31 15:50:31 -0500

    AlmostFinished

--- labs/lab-2-javascript-game/script.js
+++ labs/lab-2-javascript-game/script.js
@@ -1,32 +1,44 @@
-function gameStart(){
-    const play = window.prompt("Do you want to play the game?");
-    if (play === "yes"){
-        var name =window.prompt("Please enter your name:");
-        startFight(name);
-    }
-
-}
-
 function getDamage(damage) {
     return Math.floor(Math.random() * 10);
 
 }
 
-function startFight(){
-    var winTotal = 0;
-    var enemyHP = 15;
-    var userHP = 50;
-    var anotherAttack;
-    var rematch;
-
-
-    while(winTotal < 5 && (userHP > 0)){
-        rematch = window.prompt("Would you like to attack again?");
-        if (anotherAttack === "yes"){
-            userHP = userHP -
-
-
-        }
+function startGame() {
+    const play = window.prompt("Do you want to play the game?");
+    if (play === "Yes") {
+        var name = window.prompt("Please enter your name:");
+        startCombat(name);
     }
 
+    function startCombat() {
+        var winTotal = 0;
+        var grantHP = 10;
+        var userHP = 40;
+        var anotherAction;
+
+        while (winTotal < 5 && (userHP > 0)) {
+            anotherAction = window.prompt("Would you like to attack again?");
+            if (anotherAction === "Yes") {
+                userHP = userHP - getDamage();
+                grantHP = grantHP - getDamage();
+                console.log(name + " has " + userHP + " HP left.");
+                console.log(" Your opponent has " + grantHP + " HP left");
+            }
+            else if (anotherAction === "No") {
+                console.log(name + "  won  " + winTotal + " times.");
+
+            }
+
+            if (grantHP <= 0) {
+                winTotal = winTotal + 1;
+                grantHP = 10;
+                userHP = 40;
+            }
+
+            if (winTotal >= 5) {
+                console.log("Congratulations you won");
+
+            }
+        }
+    }
   }
