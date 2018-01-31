d16597b9b2207c8eea88f07bd6a6e7781f47de9f
NobleCar <noblejohnson1@hotmail.com>
2018-01-31 17:18:57 -0500

    FinalVersion

--- labs/lab-2-javascript-game/script.js
+++ labs/lab-2-javascript-game/script.js
@@ -17,15 +17,17 @@ function startGame() {
         var anotherAction;
 
         while (winTotal < 5 && (userHP > 0)) {
-            anotherAction = window.prompt("Would you like to attack again?");
-            if (anotherAction === "Yes") {
+            anotherAction = window.prompt("Would you like to attack again or quit?");
+            if (anotherAction === "attack") {
                 userHP = userHP - getDamage();
                 grantHP = grantHP - getDamage();
                 console.log(name + " has " + userHP + " HP left.");
                 console.log(" Your opponent has " + grantHP + " HP left");
             }
-            else if (anotherAction === "No") {
-                console.log(name + "  won  " + winTotal + " times.");
+
+
+            else if (anotherAction === "quit") {
+                return console.log(name + " Ran away with " + winTotal + " rounds.");
 
             }
 
@@ -36,7 +38,7 @@ function startGame() {
             }
 
             if (winTotal >= 5) {
-                console.log("Congratulations you won");
+                return console.log("Congratulations you won");
 
             }
         }
