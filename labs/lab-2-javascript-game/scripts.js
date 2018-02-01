5eb096476b8f65e1ac95d36a8f99375745bf751e
emilywestenbroek <em.westenbroek@gmail.com>
2018-01-31 21:00:33 -0500

    Change && and || symbols in primary while loop. Update attack prompt to terminate if anything other than attack is entered.

--- labs/lab-2-javascript-game/scripts.js
+++ labs/lab-2-javascript-game/scripts.js
@@ -1,36 +1,41 @@
 function startGame() {
   var start = prompt("Would you like to play a game against the Almighty Grant?");
   if (start === "yes") {
-   var userName = prompt("What is your name?");
-   startCombat(userName);
- }
+    var userName = prompt("What is your name?");
+    startCombat(userName);
+  }
 }
 
 function getDamage(damage) {
-    return Math.floor(Math.random() * 5) + 1;
-  }
+  return Math.floor(Math.random() * 5) + 1;
+}
 
 function startCombat(userName) {
   var grantPoints = 10;
   var userPoints = 40;
   var grantNumberWins = 0;
   var userNumberWins = 0;
-  while (userNumberWins < 3 || grantNumberWins < 3) {
+  while (userNumberWins < 3 && grantNumberWins < 1) {
     while (grantPoints > 0 && userPoints > 0) {
-    grantPoints = grantPoints - getDamage();
-    userPoints = userPoints - getDamage();
-    console.log("Grant has " + grantPoints + " health left." );
-    console.log(userName + " has " + userPoints + " health left." );
-    var attack = prompt("Do you want to quit or attack?");
-  } if (grantPoints <= 0) {
+      grantPoints = grantPoints - getDamage();
+      userPoints = userPoints - getDamage();
+      console.log("Grant has " + grantPoints + " health left." );
+      console.log(userName + " has " + userPoints + " health left.");
+      var attack = prompt("Do you want to quit or attack?");
+      if (attack !== "attack") {
+        return;
+      }
+    }
+
+    if (grantPoints <= 0) {
       userNumberWins++;
       grantPoints = 10;
-    } if (userPoints <= 0) {
-        grantNumberWins++;
-    } else if (userNumberWins >= 3) {
-          console.log("You have won!");
-    } else if (grantNumberWins >= 3) {
-          console.log("Grant has won!");
-      }
-}
+    }
+  }
+
+  if (userNumberWins >= 3) {
+    console.log("You have won!");
+  } else if (grantNumberWins = 1) {
+    console.log("Grant has won!");
+  }
 }
