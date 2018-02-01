149a36372a444a7ade67ea8953eb3c8c748ebb52
Eli Cecil <hello@elicecil.art>
2018-02-01 18:00:26 -0500

    Reworked all functions to be methods

--- labs/lab-2-javascript-game/elicecilgame.js
+++ labs/lab-2-javascript-game/elicecilgame.js
@@ -5,50 +5,77 @@ class Player {
         this.healsRemaining = healsRemaining;
         this.wins = wins;
     }
-    startGame() {
-        var start = prompt("Would you like to fight the Champion?");
-        if(start === "yes") {
-            userName = prompt("Tell us your name.");
-        }
-        startCombat();
+
+    generateAttackDamage(damage) {
+        damage = Math.round(Math.random() * 3);
+        this.health = this.health - damage;       
+        return this.health;
     }
-    generateAttackDamage() {
-        var damage = Math.round(Math.random() * 3);
-        return damage;        
-    }
+
     heal() {
-        var heal = Math.round(Math.random() * 10);  
-        return heal;
-        this.healsRemaining - 1;      
+        heal = Math.round(Math.random() * 10);  
+        this.health = this.health + heal;
+        this.healsRemaining - 1;   
+        return this.health;   
     }
+
     hitGrant() {
-        grant.health = (grant.ealth - generateAttackDamage());
+        grant.health = (grant.health - generateAttackDamage());
         console.log("Grant is now at", grant.health, "HP!");  
     }
+
     hitUser() {
         user.health = (user.health - generateAttackDamage());
         console.log(user.name, "is now at", user.health, "HP!");
     }   
+
     checkIfWin() {
         if (grant.health === 0) {
             user.checkIfWin = (user.checkIfWin + 1);
             console.log(user.name, "has", user.wins, "wins!");
         }
     }
+
     checkIfLoss() {
-        if (user.heatlh === 0) {
+        if (user.health === 0) {
             console.log(user.name, "has been defeated!");
         }
     }
-    oneDeath() {
+}
+
+
+
+function startGame() {
+    var start = prompt("Would you like to fight the Champion?");
+    var user = new Player("userName", 40, 2, 0);
+    const grant = new Player("Grant", 10, 0, 0);
+    
+    if(start === "yes") {
+        user = prompt("Tell us your name."); 
+    }
+    function oneDeath() {
         while((grant.health > 0) && (user.health > 0 )) {
-            hitUser();
-            hitGrant();
+            user.hitUser();
+            grant.hitGrant();
+            checkIfLoss();
         }
     }
+    while((user.health > 0) && (user.wins !== 3) && (again !== false)) {
+        oneDeath();
+        checkIfWin();
+        grant.health = 10;
+        var again = confirm("Would you like to attack once more or show mercy?")
+        if(again = again === "attack") {
+            attack = true;
+        } else {
+            false;
+        }
+    }
+}
+
     // while(user.health > 0) && (user.wins !== 3) && (again !== false) {
     //     oneDeath();
-    //     wins();
+    //     checkIfWin();
     //     grant.health = 10;
     //     var again = confirm("Would you like to attack once more or show mercy?")
     //     if(again = again === "attack") {
@@ -56,16 +83,11 @@ class Player {
     //     } else {
     //         false;
     //     }
-    // // }
+    // }
     // console.log(user.name, "has defeated Champion Grant, and shall take his place in history!");
-}
-
-const user = new Player("userName", 40, 2, 0);
-const grant = new Player("Grant", 10, 0, 0);
-
 // const startCombat = function() {   
 //     const hitGrant = function() {
-//         grant.health = (grant.ealth - generateAttackDamage());
+//         grant.health = (grant.health - generateAttackDamage());
 //         console.log("Grant is now at", grant.health, "HP!");  
 //     }
 //     const hitUser = function() {
@@ -79,7 +101,7 @@ const grant = new Player("Grant", 10, 0, 0);
 //         }
 //     }
 //     const checkIfLoss = function() {
-//         if (user.heatlh === 0) {
+//         if (user.health === 0) {
 //             console.log(user.name, "has been defeated!");
 //         }
 //     }
@@ -87,17 +109,6 @@ const grant = new Player("Grant", 10, 0, 0);
 //         while((grant.health > 0) && (user.health > 0 )) {
 //             hitUser();
 //             hitGrant();
-//         }
-//     }
-//     while((user.health > 0) && (user.wins !== 3) && (again !== false)) {
-//         oneDeath();
-//         wins();
-//         grant.health = 10;
-//         var again = confirm("Would you like to attack once more or show mercy?")
-//         if(again = again === "attack") {
-//             attack = true;
-//         } else {
-//             false;
 //         }
 //     }
 //     console.log(user.name, "has defeated Champion Grant, and shall take his place in history!");
