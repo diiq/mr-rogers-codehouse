c0ddb5798312a5525fdf6596d88987092b1913e9
Sam Bleckley <sam@climatum.com>
2018-01-31 20:11:42 -0500

    Sam's working of the day's exercises

--- exercises/dom-and-events/exercise.js
+++ exercises/dom-and-events/exercise.js
@@ -9,15 +9,54 @@
 // AND THEN FIX exercise.html TO USE exercise-yourname.js INSTEAD.
 
 // A function that returns the value of the input
-
+function yourName() {
+  return document.getElementById("first-input").value;
+}
 
 // A function that returns the text of the button
-
+function buttonText(){
+  return document.getElementById("go-button").innerText;
+}
 
 // A function that sets the greeting to be "Hello, [value of input]!"
+function newGreeting(){
+  document.getElementById("greeting");
+  document.getElementById("greeting").innerText = "Hello, " + yourName() + "!";
+}
+
+// Creating new elements
+function addButton() {
+  const button = document.createElement("button");
+  const divs = document.getElementsByClassName("bordered");
+  const div = divs[divs.length - 1];
+  div.appendChild(button);
+  button.innerText = "Baby Button";
+}
 
 
 // A function that runs when the button is clicked (moused-over, focused, etc)
+// document.getElementById("go-button").addEventListener("click", newGreeting);
+// Doesn't work because button isn't defined yet.
 
+// These all work, but are more verbose:
+//
+// function hookUpButton() {
+//   document.getElementById("go-button").addEventListener("click", newGreeting);
+// }
+//
+///// OR
+//
+// const hookUpButton = () => {
+//   document.getElementById("go-button").addEventListener("click", newGreeting); 
+// }
+
+// window.addEventListener("load", hookUpButton);
 
 // Onload.
+window.addEventListener("load", () => {
+  document.getElementById("go-button").addEventListener("click", newGreeting);
+  document.getElementById("add-button").addEventListener("click", addButton); 
+});
+
+
+
