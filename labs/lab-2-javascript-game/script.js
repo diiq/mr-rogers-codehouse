6f1f8a6c932f7156ee7a76e8c6242277bc6e88e2
Vikram Badarinath <vbad23@gmail.com>
2018-02-01 20:10:08 -0500

    No changes. Popped stash.

--- labs/lab-2-javascript-game/script.js
+++ labs/lab-2-javascript-game/script.js
@@ -6,34 +6,50 @@ function startGame() {
   }
 }
 
-function getDamage() {
-  const min = 1;
-  const max = 5;
-  return (Math.floor(Math.random() * (max - min + 1) + min));
+class Character {
+    constructor (name, health) {
+      this.name = name;
+      this.health = health;
+      this.healsRemaining = 2;
+      this.wins = 0;
+    }
+
+    generateAttackDamage() {
+      const min = 1;
+      const max = 3;
+      return (Math.floor(Math.random() * (max - min + 1) + min));
+    }
+
+    heal() {
+      const min = 1;
+      const max = 10;
+      this.health = this.health + (Math.floor(Math.random() * (max - min + 1) + min));
+      this.healsRemaining = this.healsRemaining - 1;
+    }
 }
 
-function battleResults(name, userHP, winCount, quit) {
+const user = new Character(name, 40);
+const grant = new Character("Grant the Mighty Chicken", 10);
+
+function battleResults(user, quit) {
   if (quit === true) {
-    console.log(name + " has quit the game.");
-  } else if (userHP <= 0 && winCount === 3) {
+    console.log(user.name + " has quit the game.");
+  } else if (user.health <= 0 && user.wins === 3) {
     console.log("The game ends in a draw.");
-  } else if (userHP <= 0) {
-    console.log(name + " has lost the game.");
-  } else if (winCount === 3) {
-    console.log(name + " has defeated Grant the Mighty Chicken!");
+  } else if (user.health <= 0) {
+    console.log(user.name + " has lost the game.");
+  } else if (user.wins === 3) {
+    console.log(user.name + " has defeated Grant the Mighty Chicken!");
   }
 }
 
-function winRound(winCount) {
-  if (winCount !== 3) { // Note: Does not show if game has finished.
-    console.log("You beat Grant! You need to win " + (3 - winCount) + " more round(s).");
+function winRound(user) {
+  if (user.wins !== 3) { // Note: Does not show if game has finished.
+    console.log("You beat Grant! You need to win " + (3 - user.wins) + " more round(s).");
   }
 }
 
 function startCombat(name) {
-  var winCount = 0;
-  var userHP = 40;
-  var grantHP = 10;
   var quit = false;
   var choice;
 
