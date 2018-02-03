3716502afd7494c6c48056b8b24f6243d151f61c
ihomebrew2 <ihomebrew2@yahoo.com>
2018-02-02 20:02:44 -0500

    Add html and stylesheets. Added eventListener to start game

--- labs/lab-2-javascript-game/javascript.js
+++ labs/lab-2-javascript-game/javascript.js
@@ -3,6 +3,20 @@ var grantHealth = 10;
 var wins = 0;
 var playGame = true;
 var battleName = "";
+var enemyName = "";
+var z = 0;
+
+
+window.addEventListener("load", () => {
+  document.getElementById("initialize").addEventListener("click", startGame);
+
+});
+
+
+function yourName() {
+  return document.getElementById("first-input").value;
+}
+
 
 class Player {
   constructor(name) {
@@ -11,29 +25,58 @@ class Player {
     this.heals = 2;
     this.wins = 0;
   }
-  generateAttackeDamage() {
+  attackDamage() {
     this.score = this.score - getDamage();
+  }
+  heals() {
+    this.score = this.score + getHealing();
   }
 }
 
+
+class Enemy {
+	constructor(enemyName) {
+		this.name = enemyName;
+		this.score = 10;
+	}
+  attackDamage() {
+    this.score = this.score - getDamage();
+  }
+
+}
 
 function startGame() {
   var playGame = confirm("Shall we battle!");
   if (playGame === true) {
     const battleName = battleTitle();
+	const enemyName = enemyTitle();
     const userPlayer = new Player(battleName);
+	const enemyPlayer = new Enemy(enemyName);
     console.log(userPlayer);
+	console.log(enemyPlayer);
   } else alert("Return when you have obtained the courage to battle!!");
 }
-startGame();
+
 
 function battleTitle() {
   return prompt("Enter your battle name!");
 }
 
-function getDamage() {
-  return Math.floor(Math.random() * 5) + 1;
+function enemyTitle() {
+  return prompt("Who shall you battle!");
 }
+
+// function getDamage() {
+  // return Math.floor(Math.random() * 5) + 1;
+// }
+
+getDamage = () => { return Math.floor(Math.random() * 3) + 1 };
+
+getHealing = () => { return Math.floor(Math.random() * 10) + 1} ;
+
+// function getHealing() {
+	// return Math.floor(Math.random() *2) +1;
+// }
 
 function attack(battleAnswer) {
   grantHealth -= getDamage();
@@ -56,20 +99,38 @@ function battleTally() {
   }
 }
 
+function battleRound() {
+  attack();
+  battleLog();
+  battleTally();
+}
+
+function battleOutput() {
+  if (wins === 3) {
+    console.log(battleName + " beat Grant 3 times, and won the game!");
+    break;
+  } else {
+    alert("You have retreated!");
+    console.log("You have retreated!");
+    break;
+  }
+}
+
 function startCombat() {
   while (playGame === true) {
     var battleAnswer = confirm("OK for attack, Cancel for retreat");
     if (battleAnswer === true) {
+
       if (wins === 3) {
         console.log(battleName + " beat Grant 3 times, and won the game!");
         break;
-      } else
-      attack(battleAnswer);
-      battleLog();
-      battleTally();
+      } else {
+        battleRound();
+      }
+
     } else {
-      alert("Coward! Grant has destroyed you!");
-      console.log("Coward! Grant has destroyed you!");
+      alert("You have retreated!");
+      console.log("You have retreated!");
       break;
     }
   }
