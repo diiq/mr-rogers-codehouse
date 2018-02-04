0812a75e4cc1dd3a850290e87ad60fac83b11029
Eli Cecil <hello@elicecil.art>
2018-02-03 21:47:43 -0500

    Continuing DOM manipulation and cleaned up code

--- labs/lab-2-javascript-game/elicecilgame.js
+++ labs/lab-2-javascript-game/elicecilgame.js
@@ -29,22 +29,28 @@ class Player {
 
 function startGame() {
     var attack = true;
-    userName = "user";
-    var user = new Player(userName, 40, 2, 0);
-    const grant = new Player("Grant", 10, 0, 0);
-    
     var start = confirm("Would you like to fight the Champion?");
     var userName = prompt("Tell us your name."); 
+    
+    var user = new Player(userName, 40, 2, 0);
+    const grant = new Player("Grant", 10, 0, 0);
 
-    function oneDeath() {
-        while((grant.health > 0) && (user.health > 0 )) {
+    function updateHealth(user, grant) {
+        document.getElementById("healthcounter").innerText = user.name + " has " + user.health + " health left. Grant has " + grant.health + " health left. ";
+    }
+    
+    document.getElementById("statsName").innerText = userName;
+
+    document.getElementById("attack").addEventListener("click", combatSequence);
+    function combatSequence() {
+        if((grant.health > 0) && (user.health > 0 )) {
             var userDamage = user.generateAttackDamage();
             var grantDamage = grant.generateAttackDamage();
             user.hitPlayer(grantDamage);
-            grant.hitPlayer(userDamage);
-            console.log(user.name, "has", user.health, "health remaining!");  
-            console.log(grant.name,  "has", grant.health,  "health remaining!"); 
-            var attackAgain = confirm("Attack again?");         
+            grant.hitPlayer(userDamage); 
+            updateHealth(user, grant);
+        } else {
+            checkIfWin();
         }
     }
 
@@ -52,33 +58,24 @@ function startGame() {
         if (grant.health <= 0) {
             user.wins = (user.wins + 1);
             console.log(user.name, "has", user.wins, "wins!");
-            return user.wins;
         } else if(user.health === 0) {
             user.health = 0;
             console.log(userName + "has been defeated!");
-            return user.health;
-        } else {
-            return;
         }
     }
 
-    while((user.health > 0) && (user.wins !== 3) && (attack === true)) {
-        oneDeath();
-        checkIfWin();
-        var attack = confirm("Would you like to attack once more or show mercy?")
-        if(attack !== true) {
-            console.log("The battle has ended.");
+    if((user.health > 0) && (user.wins !== 3)) {
+        combatSequence();
         } else {
-            grant.health = 10;
-            var playerHeal = confirm("Would you like to heal?");
-            if((playerHeal === true) && (user.healsRemaining !== 0)) {
-                var userHeal = user.heal();
-                user.healPlayer(userHeal);
-                console.log("You have", user.healsRemaining, "heals remaining.");
-            }
+            grant.health = 10;            
+            // var playerHeal = document.getElementById("heal").addEventListener("click", healPlayer);
+            // if((playerHeal === true) && (user.healsRemaining !== 0)) {
+            //     var userHeal = user.heal();
+            //     user.healPlayer(userHeal);
+            //     console.log("You have", user.healsRemaining, "heals remaining.");
+            // }
 
-        }
-    } 
+        } 
 }
 
 window.addEventListener("load", () => {
