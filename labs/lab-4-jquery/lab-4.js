c7e3cc704d8fd664df82e12e2233c8d64db1b2b2
emilywestenbroek <em.westenbroek@gmail.com>
2018-02-08 18:59:40 -0500

    Add methods to restaurant class

--- labs/lab-4-jquery/lab-4.js
+++ labs/lab-4-jquery/lab-4.js
@@ -7,21 +7,15 @@ class Party {
 }
 
 class Table {
-  constructor(available, number, position, party){
+  constructor(available, number, position){
     this.available = true;
     this.number = number;
     this.position = position;
     this.party = null;
   }
-  reserveTable() {
-    // just create a party and change status
+  reserveTable(name, contact, size) {
     this.party = new Party(name, contact, size);
     this.available = false;
-      }
-  render() {
-    
-  }
-    }
   }
 }
 
@@ -29,5 +23,14 @@ class Restaurant  {
   constructor(height, width){
     this.height = height;
     this.width = width;
+    this.tables = [];
+    this.createTables();
   }
+  createTables() {
+    for (var i = 0; i < (this.height * this.width); i++) {
+      this.tables.push(new Table(true, i, ""));
+    }
+  }
+
+
 }
