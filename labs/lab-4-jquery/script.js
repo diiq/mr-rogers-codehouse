6a7e8fb10b134548b6402a0bb4b22f137458efa3
Malcolm Cusack <cusackmalk@gmail.com>
2018-02-08 16:38:09 -0500

    First commits. Starting lab by making classes and methods

---
+++ labs/lab-4-jquery/script.js
@@ -0,0 +1,32 @@
+//make new branch. get out of master, whoops
+
+class Restaurant {
+	constructor(height, width) {
+		this.table = [];
+		this.height = height;
+		this.width = width;
+	}
+
+}
+
+class Table {
+	constructor(tableNumber, party) {
+		this.tableNumber = tableNumber;
+		this.party = party;
+		this.status = 'avalible';
+	}
+
+	changeColor() {
+		if (this.status = 'reserved') {
+			
+	}
+	}
+}
+
+class Party {
+	constructor(name, partySize, phoneNumber ) {
+		this.name = name;
+		this.partySize = partySize;
+		this.phoneNumber = phoneNumber;
+	}
+}
