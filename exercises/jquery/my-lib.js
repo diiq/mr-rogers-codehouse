c85c3c342d338be68ed73c7c59208ae5ec17f944
Sam Bleckley <sam@climatum.com>
2018-02-08 15:24:52 -0500

    Add name presence constraint

--- exercises/jquery/my-lib.js
+++ exercises/jquery/my-lib.js
@@ -1,3 +1,22 @@
+// Teacher notes: Invent jquery, one step at a time. Propose the problem: Say,
+// "it's exausting doing doc.getElementById(...).style.display = "none" just to
+// hide something. I want a faster way!" 
+//
+// - Students should suggest a hide(elt) or a hide(id) function. Write that
+//   function; 
+//
+// - coach them into developing a class (or a prototype, whatever suits your
+//   stease) collecting many such functions; 
+//
+// - show how, if each method returns `this`, you can chain methods one after
+//   another. 
+//
+// - Finally, write a factory to simplify calling `new MyLib(id)`; call the
+//   factory $.
+//
+// You've invented jquery! Introduce real jquery as just this same idea taken
+// much further.
+
 class MyLib {
   constructor(id) {
     this.elt = document.getElementById(id);
@@ -27,3 +46,5 @@ class MyLib {
 function $(id) {
   return new MyLib(id);
 }
+
+// 
