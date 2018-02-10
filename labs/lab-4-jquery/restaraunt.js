04b00ca2d32d5e26776e7deec75a1885be095f57
Eli Cecil <hello@elicecil.art>
2018-02-09 20:40:45 -0500

    Finished basic functionality

--- labs/lab-4-jquery/restaraunt.js
+++ labs/lab-4-jquery/restaraunt.js
@@ -4,13 +4,4 @@ class Restaurant {
         this.height = height;
         this.cellSize;
     }
-
-    render(parent) {
-        parent.innerHTML = "";
-        const grid = document.createElement("div");
-        grid.className = "grid";
-        grid.style.gridTemplateColumns = `repeat(${this.width}px, ${this.cellSize}px)`;
-        grid.style.gridTemplateRows = `repeat(${this.height}px, ${this.cellSize}px)`;
-        parent.appendChild(grid);
-    }
 }
--- labs/lab-4-jquery/table.js
+++ labs/lab-4-jquery/table.js
@@ -1,8 +1,7 @@
 class Table {
-    constructor(tableNum, status, reserver) {
+    constructor(tableNum, status) {
         this.tableNum = tableNum;
         this.status = status;
-        this.reserver = reserver;
     }
 
     reserveTable(name, phoneNum, peopleNum) {
@@ -11,8 +10,31 @@ class Table {
     }
 
     openTable() {
-        this.status = "available";
-        console.log(this.status);        
+        this.status = "available";  
+        this.reserver = null;
+    }
+
+    render() {
+        var _this = this;
+        var $table = $("#table" + this.tableNum);
+        $table.click(function () {
+            if(_this.status === "available") {
+                var $modalBG = $("#modal-background");
+                $modalBG.css("display", "flex");
+                $(".tab-num").text("Table number: " + _this.tableNum);
+                $(".save").click(function () {
+                    var partyName = $(".name").text();
+                    var partyNumber = $(".phone-num").text();
+                    var peopleNumber = $(".people-num").text();
+                    _this.reserveTable(partyName, partyNumber, peopleNumber);
+                    $table.css("background-color", "#898989");
+                    $modalBG.css("display", "none");
+                })
+            } else {
+                _this.openTable();
+                $table.css("background-color", "#cccccc");
+            }
+        });
     }
 }
 
