9e10bcc2b8f46b4a8c94ede3c3bb9ae63886c900
Vikram Badarinath <vbad23@gmail.com>
2018-02-09 19:01:30 -0500

    Ready for grading. Had to move data from script.js to tables.js for the form buttons to work.

--- labs/lab-4-jquery/script.js
+++ labs/lab-4-jquery/script.js
@@ -2,16 +2,4 @@ $(document).ready(function() {
 
   // DEFAULT
   $(".pop-up").hide();
-
-  // CLICK CANCEL
-  $("#cancel").click(function() {
-    $(".pop-up").slideUp();
-  });
-
-  // CLICK CLEAR
-  $("#clear").click(function() {
-    tables[tableIndex] = new Table(tableIndex);
-    $(`.table:nth-child(${tableIndex + 1})`).css("background-color", "rgb(241, 217, 188)");
-    $(".pop-up").slideUp();
-  });
 });
--- labs/lab-4-jquery/tables.js
+++ labs/lab-4-jquery/tables.js
@@ -74,4 +74,16 @@ $(document).ready(function() {
       $(".pop-up").slideUp();
     }
   });
+
+  // CLICK CANCEL
+  $("#cancel").click(function() {
+    $(".pop-up").slideUp();
+  });
+
+  // CLICK CLEAR
+  $("#clear").click(function() {
+    tables[tableIndex] = new Table(tableIndex);
+    $(`.table:nth-child(${tableIndex + 1})`).css("background-color", "rgb(241, 217, 188)");
+    $(".pop-up").slideUp();
+  });
 });
