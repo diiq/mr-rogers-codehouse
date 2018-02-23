df4a0197da8ec1d6dbc99d44cf06def8f344aa45
ihomebrew2 <ihomebrew2@yahoo.com>
2018-02-23 18:19:23 -0500

    Filter works

--- labs/lab-angularjs/form/form.component.js
+++ labs/lab-angularjs/form/form.component.js
@@ -1,7 +1,6 @@
 angular.module("todoApp").component("todoform", {
   templateUrl: "form/form.html",
   controller: ['ItemService', function(ItemService) {
-    this.items = [];
 
     this.items = function() {
       return ItemService.getItems();
