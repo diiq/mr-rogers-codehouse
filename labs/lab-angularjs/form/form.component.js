fb98d4ae15f8216da22ef849364a1da79598064a
ihomebrew2 <ihomebrew2@yahoo.com>
2018-02-23 17:03:28 -0500

    List is on page, but no items show up.

--- labs/lab-angularjs/form/form.component.js
+++ labs/lab-angularjs/form/form.component.js
@@ -1,7 +1,11 @@
 angular.module("todoApp").component("todoform", {
   templateUrl: "form/form.html",
   controller: ['ItemService', function(ItemService) {
+    this.items = [];
 
+    this.items = function() {
+      return ItemService.getItems();
+    }
 
   }]
 });
--- labs/lab-angularjs/item/item.service.js
+++ labs/lab-angularjs/item/item.service.js
@@ -1,15 +1,15 @@
 angular.module("todoApp").service("ItemService", function() {
   this.items = [];
 
+  this.getItems = function() {
+    return this.items;
+  }
+
   this.addItem = function(item) {
     this.items.push({
       item: item
     });
   console.log("Hello", this.items);
-  }
-
-  this.getItems = function() {
-    return this.items;
   }
 
 });
