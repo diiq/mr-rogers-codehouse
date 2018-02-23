c2225e13101e5704a0b0c128b1604c81ddb3bdd0
Vikram Badarinath <vbad23@gmail.com>
2018-02-23 16:57:33 -0500

    Incorporated a service. Properly shows data when switching back and forth from info page. May need to change todos into an array of objects instead of strings. Causes issues when there are duplicates.

--- labs/angular-1/todo-list/todo-list.component.js
+++ labs/angular-1/todo-list/todo-list.component.js
@@ -1,17 +1,13 @@
 angular.module("app").component("todoList", {
   templateUrl: "todo-list/todo-list.html",
-  controller: function() {
-
-    this.todos = [
-      // "Food",
-      // "Entertainment",
-      // "Clothes",
-      // "Bills"
-    ];
+  controller: ["TodoListService", function(TodoListService) {
+  this.todos = TodoListService.getTodos();
+  this.hold = ""; // Used to store todo in order to clear the input field after entry
 
     this.addTodo = function() {
-      this.todos.push(this.todo);
+      this.hold = this.todo;
       this.todo = "";
+      return TodoListService.addTodo(this.hold);
     }
-  }
+  }]
 });
